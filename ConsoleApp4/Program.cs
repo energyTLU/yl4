b701d14ff93e2c724f99d319ab1ab53b2cbb0eca
energyTLU <60502472+energyTLU@users.noreply.github.com>
2022-01-28 23:08:12 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConsoleApp4
+{
+    
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Display();
+
+        }
+        
+
+    }
+}
